62623f6f1c76e91d8023c477e2fcb1c3e985f5d6
Piotr Konowrocki <p.konowrocki@gmail.com>
2024-05-26 00:18:26 +0200

    svgr types declaration

--- app/components/ui/tile.tsx
+++ app/components/ui/tile.tsx
@@ -9,7 +9,7 @@ import {Arrow, arrowHoverParent, Btn, btnHoverParent, Tag, tagHoverParent} from 
 import {theme} from '@/app/styles'
 import {getFormattedDateRange} from '@/app/utils'
 
-interface ITileProps {
+export interface ITileProps {
   button?: string
   date?: Dayjs | [Dayjs, Dayjs]
   image?: string
