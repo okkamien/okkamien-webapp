ff9f5217ef2572555cead6962782346ab99c168f
Piotr Konowrocki <p.konowrocki@gmail.com>
2024-06-04 19:24:08 +0200

    Fix Vercel previews (#13)

--- pages/facilities/index.tsx
+++ pages/facilities/index.tsx
@@ -60,7 +60,7 @@ export const getServerSideProps: GetServerSideProps = async ({req}) => {
   const ids = facilities?.data.map(({id}) => id) ?? []
 
   const payloads: IGetApiCollectionResponseParams<TApiWorkshop>[] = [
-    {endpoint: 'facilities', filters: {id: [ids, 'contains']}, populate: ['thumbnail']},
+    {endpoint: 'facilities', filters: {id: [ids, 'containsi']}, populate: ['thumbnail']},
   ]
   const {dehydratedState} = await getDehydratedState({payloads, req})
 
