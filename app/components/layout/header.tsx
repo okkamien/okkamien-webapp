6272d02dcfdbd8cd5371a346a32efbcf70c0526a
Michał Dawid <michal11dawid@gmail.com>
2024-06-18 19:19:12 +0200

    Add hamburger menu and pixelPerfect

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,28 +1,36 @@
 import React, {FC, useEffect, useState} from 'react'
 import {Anchor, Box} from '@effortless-ui'
+import {IconChevronRight, IconMenu2, IconX} from '@tabler/icons-react'
+import Image from 'next/image'
 import Link from 'next/link'
 
-import LogoNavIcon from '@/app/assets/Logo-nav.svg'
-import LogoNavFixedIcon from '@/app/assets/Logo-nav-fixed.svg'
 import {Container} from '@/app/components/ui'
-import {navigationLinks, navigationSocials, siteMap} from '@/app/dictionaries/site.dictionary'
+import {navigationLinks, navigationSocials, siteMap, siteName} from '@/app/dictionaries/site.dictionary'
+import {useOutsideElementClickHandler} from '@/app/hooks/use-outside-element-click-handler'
 import {theme} from '@/app/styles'
 
 export const Header: FC = () => {
   const [scroll, setScroll] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const ref = useOutsideElementClickHandler(() => setIsMenuOpen(false))
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
       setScroll(window.scrollY > 10)
     })
-  })
+  }, [])
+
+  useEffect(() => {
+    if (isMenuOpen) document.body.style.overflow = 'hidden'
+    else document.body.style.overflow = 'auto'
+  }, [isMenuOpen])
 
   return (
     <>
       <Box
         tag="header"
         cs={{
-          py: theme.spacing.m,
+          py: [theme.spacing.ml, theme.spacing.s],
           position: scroll ? 'fixed' : 'static',
           top: 0,
           width: '100%',
@@ -38,38 +46,167 @@ export const Header: FC = () => {
             }}
           >
             <Link href={siteMap.homepage} legacyBehavior passHref>
-              {scroll ? <LogoNavFixedIcon /> : <LogoNavIcon />}
+              {scroll ? (
+                <Box
+                  cs={{
+                    label: 'navigaction-image',
+                    width: [120, 180],
+                    py: [theme.spacing.xxs, theme.spacing.s],
+                  }}
+                >
+                  <Image src="/Logo-color-b.svg" width={180} height={40} alt={siteName} sizes="100%" />
+                </Box>
+              ) : (
+                <Box
+                  cs={{
+                    label: 'navigaction-image',
+                    width: [120, 180],
+                    py: [theme.spacing.xxs, theme.spacing.s],
+                  }}
+                >
+                  <Image src="/Logo-monowhite-b.svg" width={180} height={40} alt={siteName} sizes="100%" />
+                </Box>
+              )}
             </Link>
-            <Box cs={{display: 'flex', flexDirection: 'column', alignItems: 'end', justifyContent: 'end'}}>
-              <Box tag="ul" composition={['semanticList']} cs={{display: 'flex', columnGap: theme.spacing.l, pb: theme.spacing.s}}>
-                {navigationSocials.map(({label, link}, i) => (
-                  <Box tag="li" key={i}>
-                    <Link href={link} legacyBehavior passHref>
-                      <Anchor
+            <Box
+              cs={{display: ['flex', 'none'], justifyContent: 'center', alignItems: 'center', zIndex: 4}}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              <IconMenu2 css={{color: scroll ? theme.color.black : theme.color.white}} />
+            </Box>
+            <Box
+              cs={{
+                position: ['fixed', 'static'],
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                zIndex: [isMenuOpen ? 4 : -1, 4],
+                '&::before': {
+                  content: '""',
+                  position: 'fixed',
+                  display: [isMenuOpen ? 'block' : 'none', 'none'],
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  zIndex: isMenuOpen ? 1 : -1,
+                  background: theme.color.overlay,
+                  opacity: 0.5,
+                },
+              }}
+            >
+              <Box
+                ref={ref}
+                cs={{
+                  display: [isMenuOpen ? 'flex' : 'none', 'flex'],
+                  position: ['fixed', 'static'],
+                  top: 0,
+                  right: 0,
+                  bottom: 0,
+                  left: theme.spacing.ml,
+                  p: [theme.spacing.ms, 0],
+                  zIndex: 5,
+                  borderTopLeftRadius: theme.radii.l,
+                  borderBottomLeftRadius: theme.radii.l,
+                  flexDirection: 'column',
+                  height: ['100%', 'auto'],
+                  background: [theme.color.white, 'transparent'],
+                }}
+              >
+                <Box
+                  cs={{
+                    display: ['flex', 'none'],
+                    justifyContent: 'end',
+                    alignItems: 'center',
+                    zIndex: 4,
+                    width: '100%',
+                    pt: [theme.spacing.ms, 0],
+                    mb: [theme.spacing.ml, 0],
+                  }}
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
+                  <IconX css={{color: theme.color.black}} size={22} />
+                </Box>
+                <Box
+                  cs={{
+                    display: 'flex',
+                    flexDirection: ['column-reverse', 'column'],
+                    justifyContent: 'space-between',
+                    alignItems: ['start', 'end'],
+                    height: ['100%', 'auto'],
+                    mt: theme.spacing.xxs,
+                  }}
+                >
+                  <Box
+                    tag="ul"
+                    composition={['semanticList']}
+                    cs={{
+                      display: 'flex',
+                      width: ['100%', 'auto'],
+                      columnGap: [theme.spacing.xl, theme.spacing.l],
+                      mt: [0, theme.spacing.xxs],
+                      mb: [0, theme.spacing.xs],
+                    }}
+                  >
+                    {navigationSocials.map(({label, link}, i) => (
+                      <Box tag="li" key={i}>
+                        <Link href={link} legacyBehavior passHref>
+                          <Anchor
+                            cs={{
+                              fontSize: theme.font.size.small,
+                              color: [theme.color.black, scroll ? theme.color.black : theme.color.white],
+                              textDecoration: 'none',
+                            }}
+                          >
+                            {label}
+                          </Anchor>
+                        </Link>
+                      </Box>
+                    ))}
+                  </Box>
+                  <Box
+                    tag="ul"
+                    composition={['semanticList']}
+                    cs={{
+                      display: 'flex',
+                      columnGap: theme.spacing.xxl,
+                      rowGap: theme.spacing.ml,
+                      width: ['100%', 'auto'],
+                      paddingBottom: theme.spacing.s,
+                      flexDirection: ['column', 'row'],
+                    }}
+                  >
+                    {navigationLinks.map(({label, link}, i) => (
+                      <Box
+                        tag="li"
+                        key={i}
                         cs={{
-                          fontSize: theme.font.size.small,
-                          color: scroll ? theme.color.black : theme.color.white,
-                          textDecoration: 'none',
+                          pt: theme.spacing.xs,
                         }}
                       >
-                        {label}
-                      </Anchor>
-                    </Link>
+                        <Link href={link} legacyBehavior passHref>
+                          <Anchor
+                            cs={{
+                              color: [theme.color.black, scroll ? theme.color.black : theme.color.white],
+                              textDecoration: 'none',
+                              fontSize: [theme.font.size.big, theme.font.size.base],
+                              letterSpacing: [theme.font.spacing.small, theme.font.spacing.xsm],
+                              display: ['flex', 'inline'],
+                              justifyContent: 'space-between',
+                              alignItems: 'center',
+                            }}
+                          >
+                            {label}
+                            <Box cs={{display: ['block', 'none']}}>
+                              <IconChevronRight css={{color: theme.color.black}} size={18} />
+                            </Box>
+                          </Anchor>
+                        </Link>
+                      </Box>
+                    ))}
                   </Box>
-                ))}
-              </Box>
-              <Box
-                tag="ul"
-                composition={['semanticList']}
-                cs={{display: 'flex', columnGap: theme.spacing.xxl, paddingBottom: theme.spacing.s}}
-              >
-                {navigationLinks.map(({label, link}, i) => (
-                  <Box tag="li" key={i}>
-                    <Link href={link} legacyBehavior passHref>
-                      <Anchor cs={{color: scroll ? theme.color.black : theme.color.white, textDecoration: 'none'}}>{label}</Anchor>
-                    </Link>
-                  </Box>
-                ))}
+                </Box>
               </Box>
             </Box>
           </Box>
--- app/styles/theme.ts
+++ app/styles/theme.ts
@@ -69,6 +69,7 @@ export const theme = {
   },
   font: {
     size: {
+      big: 25,
       base: 16,
       small: 13,
     },
@@ -77,6 +78,8 @@ export const theme = {
     },
     spacing: {
       base: 1.2,
+      small: 0.5,
+      xsm: 0.1,
     },
   },
   size: {
